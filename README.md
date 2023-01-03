4807637c4f6281b49d93d4b0857939876ccd76f0
timpriadko <timpriadko@gmail.com>
2023-01-03 19:21:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React-Learn-2023
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-v6kctn)
---
+++ component/TextComponent/TextComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TextComponent = () => {
+  return <div>index</div>;
+};
+
+export default TextComponent;
